e293908b95e1a97bc2a5f2f8de1818d605a620d6
Sparks0219 <73967497+Sparks0219@users.noreply.github.com>
2021-11-10 17:47:04 -0500

    Update Inverted.py

--- Inverted.py
+++ Inverted.py
@@ -19,5 +19,5 @@ class InvertedIndex:
     def __next__(self):
         return self
 
-for i, (docs, freqs) in enumerate(InvertedIndex("/home/josh/CW09B")):
+for i, (docs, freqs) in enumerate(InvertedIndex("/home/josh/CW09B/CW09B.url.inv")):
     print(i, docs, freqs)
