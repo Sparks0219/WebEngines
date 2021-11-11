db697210bc6329165fb469fef69313f6df90beab
Sparks0219 <73967497+Sparks0219@users.noreply.github.com>
2021-11-10 20:43:10 -0500

    Update Postings.py

--- Postings.py
+++ Postings.py
@@ -10,11 +10,24 @@ class InvertedIndex:
         i = 2
         while i < len(self.docs):
             size = self.docs[i]
-            yield (self.docs[i+1:size+i+1])
+            print(DeltaEncoding(self.docs[i+1:size+i+1]))
             i += size+1
-
+        
     def __next__(self):
         return self
+    
+def DeltaEncoding(postingList):
+    last = 0 
+    countBytes = 0
+    for i in range(0, length(postingList)-1, 1):
+        current = postingList[i]
+        delta = current - last;
+        last = current;
+        countBytes += delta//128
+        if (countBytes%128 != 0):
+            countBytes +=1
+    return countBytes
+        
 
 for i, docs in enumerate(InvertedIndex("/home/josh/output/output.url.inv")):
     print(i, docs)
