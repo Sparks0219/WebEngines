6e2f29780cee5468c8f51a4bdfb20fe38341ae24
Sparks0219 <73967497+Sparks0219@users.noreply.github.com>
2021-11-15 12:37:51 -0500

    Update Postings.py

--- Postings.py
+++ Postings.py
@@ -54,19 +54,19 @@ def Simple9(postingList):
         print(countBytes) 
         if (len(postingList[i::]) >= 28 and max(postingList[i:28]) <= 1):
             i+=28
-        elif (len(postingList[i::]) >= 14 and max(postingList[i:14] <= 3):
+        elif (len(postingList[i::]) >= 14 and max(postingList[i:14]) <= 3):
             i+=14
-        elif (len(postingList[i::]) >= 9 and max(postingList[i:9] <= 7):
+        elif (len(postingList[i::]) >= 9 and max(postingList[i:9]) <= 7):
             i+=9
-        elif (len(postingList[i::]) >= 7 and max(postingList[i:7] <= 15):
+        elif (len(postingList[i::]) >= 7 and max(postingList[i:7]) <= 15):
             i+=7
-        elif (len(postingList[i::]) >= 5 and max(postingList[i:5] <= 31):
+        elif (len(postingList[i::]) >= 5 and max(postingList[i:5]) <= 31):
             i+=5
-        elif (len(postingList[i::]) >= 4 and max(postingList[i:4] <= 127):
+        elif (len(postingList[i::]) >= 4 and max(postingList[i:4]) <= 127):
             i+=4
-        elif (len(postingList[i::]) >= 3 and max(postingList[i:3] <= 511):
+        elif (len(postingList[i::]) >= 3 and max(postingList[i:3]) <= 511):
             i+=3
-        elif (len(postingList[i::]) >= 2 and max(postingList[i:2] <= 16383):
+        elif (len(postingList[i::]) >= 2 and max(postingList[i:2]) <= 16383):
             i+=2 
         else: #assuming all numbers are less than 268435456 (2^28)
             i+=1 
