84cf9011b5a431088690709f6ebacaf37d61ebfd
Sparks0219 <73967497+Sparks0219@users.noreply.github.com>
2021-11-21 06:13:08 -0500

    Update Postings.py

--- Postings.py
+++ Postings.py
@@ -12,7 +12,7 @@ class InvertedIndex:
             size = self.docs[i]
             #Three tuple containing range, size of posting list, encoding
             #(self.docs[size+i]-self.docs[i],size,SomeEncoding(self.docs[i+1:size+i+1]))
-            print(self.docs[1])
+            print(self.docs[0])
             bytes = VarByteEncoding(self.docs[i+1:size+i+1])
             f.write(str(size)+" "+str(self.docs[1])+" "+str(bytes)+"\n")
             i += size+1
