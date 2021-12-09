db2772b063653799294bbe28c4cecac3f7adec4d
Sparks0219 <73967497+Sparks0219@users.noreply.github.com>
2021-12-09 03:48:03 -0500

    Update Postings.py

--- Postings.py
+++ Postings.py
@@ -67,6 +67,7 @@ def Simple9(postingList):
     for y in range(len(newList)):
         if (y!=0):
             newList[y] = postingList[y]-postingList[y-1]-1
+            print(newList[y])
     while i < len(newList):
         #print(countBytes)
         if (len(postingList)-1-i >= 28 and myMax(i,28,1,newList) == True):
